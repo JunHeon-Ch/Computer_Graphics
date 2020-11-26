41ba73b2ef1fc9d2db452ff30821c25dc9c0f785
JunHeon-Ch <51908258+JunHeon-Ch@users.noreply.github.com>
2020-11-26 17:23:24 +0900

    feat: 201126 Exercise

---
+++ Exercise/201126_#43_Hierarchical_Model_Cube/Hierarchical_Model_Cube/robotArm-empty.js
@@ -0,0 +1,193 @@
+"use strict";
+
+var canvas, gl, program;
+
+var NumVertices = 36; //(6 faces)(2 triangles/face)(3 vertices/triangle)
+
+var points = [];
+var colors = [];
+
+var vertices = [
+    vec4(-0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, -0.5, -0.5, 1.0),
+    vec4(-0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, -0.5, -0.5, 1.0)
+];
+
+// RGBA colors
+var vertexColors = [
+    vec4(0.0, 0.0, 0.0, 1.0),  // black
+    vec4(1.0, 0.0, 0.0, 1.0),  // red
+    vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+    vec4(0.0, 1.0, 0.0, 1.0),  // green
+    vec4(0.0, 0.0, 1.0, 1.0),  // blue
+    vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+    vec4(1.0, 1.0, 1.0, 1.0),  // white
+    vec4(0.0, 1.0, 1.0, 1.0)   // cyan
+];
+
+
+// Parameters controlling the size of the Robot's arm
+
+var BASE_HEIGHT = 2.0;
+var BASE_WIDTH = 5.0;
+var LOWER_ARM_HEIGHT = 5.0;
+var LOWER_ARM_WIDTH = 0.5;
+var UPPER_ARM_HEIGHT = 5.0;
+var UPPER_ARM_WIDTH = 0.5;
+
+// Shader transformation matrices
+
+var modelViewMatrix, projectionMatrix;
+
+// Array of rotation angles (in degrees) for each rotation axis
+
+var Base = 0;
+var LowerArm = 1;
+var UpperArm = 2;
+
+
+var theta = [0, 0, 0];
+
+var angle = 0;
+
+var modelViewMatrixLoc;
+
+var vBuffer, cBuffer;
+
+// quad uses first index to set color for face
+function quad(a, b, c, d) {
+    colors.push(vertexColors[a]);
+    points.push(vertices[a]);
+    colors.push(vertexColors[a]);
+    points.push(vertices[b]);
+    colors.push(vertexColors[a]);
+    points.push(vertices[c]);
+    colors.push(vertexColors[a]);
+    points.push(vertices[a]);
+    colors.push(vertexColors[a]);
+    points.push(vertices[c]);
+    colors.push(vertexColors[a]);
+    points.push(vertices[d]);
+}
+
+// Each face determines two triangles
+function colorCube() {
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(6, 5, 1, 2);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
+}
+
+// Remove when scale in MV.js supports scale matrices
+function scale4(a, b, c) {
+    var result = mat4();
+    result[0][0] = a;
+    result[1][1] = b;
+    result[2][2] = c;
+    return result;
+}
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    // Load shaders and initialize attribute buffers
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+
+    gl.useProgram(program);
+
+    colorCube();
+
+    // Load shaders and use the resulting shader program\
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    // Vertex array attribute buffer
+    // Creating a buffer to store coordinates of vertices
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+    // Associating vertex data buffer with shader variables in program
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    // Color array attribute buffer
+    // Creating a buffer to store color of vertices
+    cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    // Associating vertex data buffer with shader variables in program
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    // Slider event
+    document.getElementById("slider1").onchange = function (event) {
+        theta[0] = event.target.value;
+    };
+    document.getElementById("slider2").onchange = function (event) {
+        theta[1] = event.target.value;
+    };
+    document.getElementById("slider3").onchange = function (event) {
+        theta[2] = event.target.value;
+    };
+
+    // Uniform variable for model view matrix
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    // Uniform variable for projection matrix
+    projectionMatrix = ortho(-10, 10, -10, 10, -10, 10);
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "projectionMatrix"), false, flatten(projectionMatrix));
+
+    render();
+}
+
+// Draw base cube
+function base() {
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
+}
+
+// Draw upper arm
+function upperArm() {
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
+}
+
+// Draw lower arm
+function lowerArm() {
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
+}
+
+var render = function () {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    modelViewMatrix = rotate(theta[Base], 0, 1, 0);
+    base();
+
+    modelViewMatrix = mult(modelViewMatrix, translate(0.0, BASE_HEIGHT, 0.0));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[LowerArm], 0, 0, 1));
+    lowerArm();
+
+    modelViewMatrix = mult(modelViewMatrix, translate(0.0, LOWER_ARM_HEIGHT, 0.0));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[UpperArm], 0, 0, 1));
+    upperArm();
+
+    requestAnimFrame(render);
+}
